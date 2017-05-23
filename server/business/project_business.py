743854a9c309563c8306902615a7ce80419a001a
Chun <acrobaticat@gmail.com>
2017-05-23 17:26:44 +0800

    fix conflict in project.py

--- server/business/project_business.py
+++ server/business/project_business.py
@@ -8,6 +8,3 @@ project_repo = ProjectRepo(Project)
 
 def create(project):
     print(project.name)
-
-def get_by_project_id(project_id):
-    return project_repo.find_unique_one({'_id': project_id})
---
+++ server/service/project_service.py
@@ -0,0 +1,8 @@
+
+from datetime import datetime
+from server.business import project_business
+from server.business import user_business
+
+def get_projects_by_user_ID(user_ID):
+
+    return project_repo.find_unique_one({'_id': project_id})
