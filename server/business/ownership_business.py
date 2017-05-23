8acb6e248246141b0c4340743aef3152dd4077f0
Zhaofeng Li <zhaofengli@Zhaofengs-MacBook-Pro.local>
2017-05-23 19:37:39 +0800

    add ownership service

--- server/business/ownership_business.py
+++ server/business/ownership_business.py
@@ -16,5 +16,5 @@ def add(user, if_private, **owned_obj):
     return ownership_repo.create(ownership_obj)
 
 
-def find_ownership_by_user_ID(user_ID):
-    pass
+def list_ownership_by_user(user):
+    ownership_repo.read_by_user(user)
