5a976dec1bb36f162825a2cde3afbc42c6dc903d
小天 <sky@shoutens-MacBook-Pro.local>
2017-05-23 10:52:44 +0800

    no message

--- server/entity/toolkit.py
+++ server/entity/toolkit.py
@@ -6,7 +6,15 @@ class Toolkit(Document):
     # object_id = StringField(max_length=20, unique=True)
     name = StringField(max_length=50, unique=True)
     description = StringField(max_length=100, unique=True)
-    target_py = StringField(max_length=25, unique=True)
-    parameter_spec = DictField()
-    status = DictField()
+
+    # target_py = StringField(max_length=25, unique=True)
+    target_py = ReferenceField('File')
+
     # ownership_ref = StringField(max_length=20, unique=True)
+    # ownership_ref = ReferenceField('Ownership')
+
+    status = DictField()
+    # status = IntField(choices=STATUS, required=True)
+
+    parameter_spec = DictField()
+    
