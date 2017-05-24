735a2c781aa63f7718c918ab9faf77e57beb31a4
小天 <sky@shoutens-MacBook-Pro.local>
2017-05-24 23:43:40 +0800

    add init file import

--- server/test_zhang.py
+++ server/test_zhang.py
@@ -15,12 +15,24 @@ connect(
     password=config.get_mongo_pass(),
     host=config.get_mongo_host(),)
 
+def log(func):
+    def wrapper(*args, **kw):
+        print 'call %s()' % func.__name__
+        func(*args, **kw)
+        print 'call %s() after' % func.__name__
+        return
+    return wrapper
+
+@log
+def now(arr0, arr1, k):
+    print 'love wyt'
+
 if __name__ == '__main__':
     # toolkit.save_once()
     # a = toolkit_business.get_by_toolkit_name('平均值')
 
-    b = toolkit_business.get_by_toolkit_id(ObjectId("592546b18be34d179b95d96f"))
-    print b.to_mongo()
+    # b = toolkit_business.get_by_toolkit_id(ObjectId("592546b18be34d179b95d96f"))
+    # print b.to_mongo()
     # c = toolkit_business.list_available_toolkits()
     # print isInstance(a, class)
     # print c
@@ -28,6 +40,7 @@ if __name__ == '__main__':
     # print a.to_mongo()
     # f = open('./run.py')
     # print f.read()
-    a = [1,2,3,4,5]
-    print AVG.toolkit_average(a)
-    print STD.toolkit_std(a)
+    # a = [1,2,3,4,5]
+    # print AVG.toolkit_average(a)
+    # print STD.toolkit_std(a)
+    now([1, 2], [2, 3], 3)
