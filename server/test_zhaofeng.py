ac5f2debc64ed6c42c303b9f5c94765744dbc000
小天 <sky@shoutens-MacBook-Pro.local>
2017-05-24 18:57:11 +0800

    test conflict

--- server/test_zhaofeng.py
+++ server/test_zhaofeng.py
@@ -6,6 +6,6 @@ from business import ownership_business
 from entity.file import File
 
 data_service.import_data_from_file_object_id(ObjectId("592551a8df86b22826983bdc"), 'test_with_os', 'some ds',
-    'tttt', True)
+    'ttt', True)
 # print ownership_business.list_ownership_by_type('file')
 # s
