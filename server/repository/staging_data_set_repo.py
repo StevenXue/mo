708d018a655608444fcdd26fa23e672e2e2df1e8
Chun <acrobaticat@gmail.com>
2017-05-24 23:07:23 +0800

    staging_data_set model and functions

--- server/repository/staging_data_set_repo.py
+++ server/repository/staging_data_set_repo.py
@@ -6,3 +6,6 @@ from repository.general_repo import Repo
 class StagingDataSetRepo(Repo):
     def __init__(self, instance):
         Repo.__init__(self, instance)
+
+    def read_by_name(self, staging_data_set):
+        return Repo.read_unique_one(self, {'name': staging_data_set.name})
