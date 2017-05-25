4ff96281bb23e6385f3e3133a71d3a0c3645d02f
小天 <sky@shoutens-MacBook-Pro.local>
2017-05-25 09:06:50 +0800

    add init file import

--- server/service/toolkit_service.py
+++ server/service/toolkit_service.py
@@ -11,3 +11,29 @@
 
 import numpy as np
 import pandas as pd
+from lib import *
+
+
+def create_job_and_result(text):
+    """
+    help toolkit to create a job before toolkit runs, 
+    as well as save the job & create a result after toolkit runs
+    :param text: 
+    :return: 
+    """
+    def decorator(func):
+        def wrapper(*args, **kw):
+            print '%s %s():' % (text, func.__name__)
+            # create a job
+            func(*args, **kw)
+            # update a job
+            # create a result
+            #return fuction_result
+        return wrapper
+    return decorator
+
+
+# Further FIXME to check whether toolkit name/id is input
+@create_job('toolkit_name')
+def now():
+    print '2013-12-25'
