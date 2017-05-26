6a0a18dd5bd2c6e994429ba6fb0b868246946ca4
Chun <acrobaticat@gmail.com>
2017-05-26 15:41:23 +0800

    Edited project_business.py

--- server/business/project_business.py
+++ server/business/project_business.py
@@ -10,6 +10,11 @@ def create(project):
     return project_repo.create(project)
 
 
+def add(name, description):
+    project_obj = Project(name=name, description=description)
+    return project_repo.create(project_obj)
+
+
 def get_by_id(object_id):
     project = Project(id=object_id)
     return project_repo.read_by_id(project)
