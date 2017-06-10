ff10063b7046c503ad06cc878364067a86ac2461
Zhaofeng Li <zhaofengli@Zhaofengs-MacBook-Pro.local>
2017-06-10 22:02:05 +0800

    node static server

---
+++ client/web.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static('./dist'));
+// app.use(express.static('./assets'));
+
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, './dist', 'stats.html'));
+});
+
+app.listen(3090, "0.0.0.0");
