59023de53ae7e5adcae55c61a820e82219d91873
小天 <sky@shoutens-MacBook-Pro.local>
2017-06-12 11:04:16 +0800

    添加python2，python3版本判断

--- server/entity/job.py
+++ server/entity/job.py
@@ -22,8 +22,9 @@ STATUS = (
 class Job(Document):
     model = ReferenceField('Model')
     toolkit = ReferenceField('Toolkit')
-    staging_data_set = ReferenceField('StagingDataSet', required=True)
-    # staging_data_set = StringField('StagingDataSet')
+    # TODO FIXME very bad
+    # staging_data_set = ReferenceField('StagingDataSet', required=True)
+    staging_data_set = StringField('StagingDataSet')
     status = IntField(choices=STATUS, required=True)
     create_time = DateTimeField(required=True)
     updated_time = DateTimeField()
