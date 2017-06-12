fca8aa94d9db1fb2f9000a1886da0ee40c203b4a
小天 <sky@shoutens-MacBook-Pro.local>
2017-06-12 11:03:45 +0800

    添加python2，python3版本判断

--- server/utility/data_utility.py
+++ server/utility/data_utility.py
@@ -48,6 +48,12 @@ def str_to_float(s):
 
 
 def convert_string_to_number(s):
+    """
+    just convert string to number, if not possibility, raise error
+    @author   : Tianyi Zhang
+    :param s: input string
+    :return: int or float of raise error
+    """
     try:
         return int(s)
     except ValueError:
@@ -55,6 +61,12 @@ def convert_string_to_number(s):
 
 
 def convert_string_to_number_with_poss(s):
+    """
+    just convert string to number, if not possibility, keep string itself
+    @author   : Tianyi Zhang
+    :param s: input string
+    :return: np.nan(not a number), int or float, or string
+    """
     if s == "":
         return np.nan
     else:
@@ -65,3 +77,22 @@ def convert_string_to_number_with_poss(s):
                 return float(s)
             except ValueError:
                 return s
+
+
+def k_fold_cross_validation(data_train, data_target, ratio=0.1):
+    """
+    k-fold cross validation, split the training data into two part:
+    one of it is for training
+    the other is for testing
+    @author   : Tianyi Zhang
+    :param data_train: raw training data
+    :param data_target: raw training target
+    :param ratio: number of training data/testing data
+    :return: X_train, X_test, y_train, y_test
+    """
+    from sklearn.model_selection import train_test_split
+    from random import randint
+    ramdom_state = randint(1,100)
+    x_train, x_test, y_train, y_test = train_test_split(data_train, data_target,
+                                                        test_size=ratio, random_state=ramdom_state)
+    return x_train, x_test, y_train, y_test
