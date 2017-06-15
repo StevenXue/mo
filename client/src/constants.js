e05445d593725c30c55f005fcc3a59d1359a660f
taoyiran <gerardtao0315@gmail.com>
2017-06-15 14:33:14 +0800

    nc

--- client/src/constants.js
+++ client/src/constants.js
@@ -3,6 +3,6 @@ module.exports = {
   // jupyterServer: 'http://10.52.14.182:8888/api/contents/',
 
   // server api
-  flaskServer: 'http://10.52.14.182:5000',
-  //flaskServer: 'http://localhost:5000',
+  //flaskServer: 'http://10.52.14.182:5000',
+  flaskServer: 'http://localhost:5000',
 }
