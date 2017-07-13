ddf03a6081edbff0544325b91fe97c5c7fa1e465
小天 <sky@shoutens-MacBook-Pro.local>
2017-07-13 20:46:33 +0800

    no message

--- pyserver/server3/service/visualization_service.py
+++ pyserver/server3/service/visualization_service.py
@@ -25,14 +25,14 @@ def usr_story1_exploration(data, d_type, group_num=10):
         arr_temp = [float(list(arr.values())[0]) for arr in data if isNaN(list(arr.values())[0]) == False and list(arr.values())[0] != '']
     else:
         arr_temp = [list(arr.values())[0] for arr in data if isNaN(list(arr.values())[0]) == False and list(arr.values())[0] != '']
-    print (arr_temp)
+    print ('arr_temp', arr_temp)
 
     # 生成的dict有4栏:
     # 数值类型 'type',
     # 基本信息 'gen_info', 产生一些数据的基本信息
     # 频率直方图/词云图 'freq_hist'
     # 假设检验的信息 'hypo_info'
-    gen_info = generate_stats_info(arr_temp)
+    gen_info = generate_stats_info(arr_temp, d_type)
     info_dict = {'type': d_type, 'gen_info': gen_info}
     if d_type == 'int' or d_type == 'float':
         arr_array = np.array(arr_temp)
@@ -51,15 +51,22 @@ def usr_story1_exploration(data, d_type, group_num=10):
         flag, p_value, standard_norm_value = hypo_test(arr_array, mean, std, x_domain, 'norm')
         info_dict['hypo'] = {'flag': flag, 'p_value': p_value, 'standard_norm_value': standard_norm_value}
     else:
-        info_dict['freq_hist'] = [{'name': key, 'value': len(list(group))} for key, group in groupby(arr_temp)]
+        seta = set(arr_temp)
+        info_dict['freq_hist'] = [{'name': el, 'value': arr_temp.count(el)} for el in seta if arr_temp.count(el) > 1]
+        # info_dict['freq_hist'] = [{'name': key, 'value': len(list(group))} for key, group in groupby(arr_temp)]
         info_dict['hypo'] = {}
 
     return info_dict
 
 
 # 用来获取数据的基本信息（长度，均值，值域等）
-def generate_stats_info(data):
-    return pd.DataFrame(data).describe().astype(float).to_dict()[0]
+def generate_stats_info(data, d_type):
+    if d_type == 'int':
+        return pd.DataFrame(data).describe().astype(int).to_dict()[0]
+    elif d_type == 'float':
+        return pd.DataFrame(data).describe().astype(float).to_dict()[0]
+    else:
+        return pd.DataFrame(data).describe().astype(str).to_dict()[0]
 
 
 # 用来做假设检验，默认是正太分布
