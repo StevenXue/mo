3598bc94e53f9e6925b107837dce993ad1e37fd0
小天 <sky@shoutens-MacBook-Pro.local>
2017-07-21 21:36:09 +0800

    no message

--- pyserver/server3/route/visualization_route.py
+++ pyserver/server3/route/visualization_route.py
@@ -13,8 +13,7 @@ from flask import request
 
 from server3.service import visualization_service
 from server3.business import staging_data_business
-# from server3.business import toolkit_business
-# from server3.business import staging_data_business
+from server3.business import staging_data_set_business
 from server3.utility import json_utility
 
 PREFIX = '/visualization'
@@ -42,15 +41,20 @@ def usr1_visualization():
 def usr2_visualization():
     data = request.get_json()
     staging_data_set_id = data.get('staging_data_set_id')
-    type = data.get('type')
+    print("hahahah")
+    # try:
+    #
+    #     data = staging_data_set_business.get_by_id(ObjectId(staging_data_set_id))["visualization"]
+    #     print("data", data)
+    #     # result = visualization_service.usr_story1_exploration(data, type)
+    #     # print ('result', result)
+    # except Exception as e:
+    #     return jsonify({'response': '%s: %s' % (str(Exception), e.args)}), 400
+    # # return jsonify({'response': json_utility.convert_to_json(data)}), 200
 
-    try:
-        data = staging_data_business.get_by_staging_data_set_and_fields(ObjectId(staging_data_set_id), field)
-        data = [d.to_mongo().to_dict() for d in data]
-        result = visualization_service.usr_story1_exploration(data, type)
-        # print ('result', result)
-    except Exception as e:
-        return jsonify({'response': '%s: %s' % (str(Exception), e.args)}), 400
+    data = staging_data_set_business.get_by_id(ObjectId(staging_data_set_id)).to_mongo().to_dict()["visualization"]
+    print("1")
+    result = visualization_service.usr_story2_exploration(data, "聚类")
     return jsonify({'response': json_utility.convert_to_json(result)}), 200
 
 
