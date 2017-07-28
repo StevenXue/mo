80b5108f9c4f8dfc963ab5c3c46558b3f0b967e4
Zhaofeng Li <zhaofengli@MacBook-Pro-LZF.local>
2017-07-28 17:48:00 +0800

    add project/query to project detail page

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,6 +1,6 @@
 module.exports = {
   jupyterServer: 'http://localhost:8888/api/contents/',
-  //jupyterServer: 'http://10.52.14.182:8888/api/contents/',
+  // jupyterServer: 'http://10.52.14.182:8888/api/contents/',
 
   // server api
   flaskServer: 'http://localhost:5000',
@@ -8,7 +8,7 @@ module.exports = {
 
   //jupyter
   baseUrl: 'http://localhost:8888',
-  //baseUrl: 'http://10.52.14.182:8888'
+  // baseUrl: 'http://10.52.14.182:8888',
 
   mainColor: '#108ee9',
 
