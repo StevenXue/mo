46adecaf11c1717cb7407dd8d9ea83b5842baaaf
Zhaofeng Li <zhaofengli@MacBook-Pro-LZF.local>
2017-07-31 16:46:16 +0800

    release for videos

--- client/web.js
+++ client/web.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 
 app.use(express.static('./dist'));
-// app.use(express.static('./assets'));
+app.use(express.static('./assets'));
 
 
 app.get('/*', function (req, res) {
