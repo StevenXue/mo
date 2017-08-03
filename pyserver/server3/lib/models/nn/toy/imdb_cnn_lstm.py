9b1f14e45de98134bc4a497fae352353a7a67814
Weston Lu <westonlu@foxmail.com>
2017-08-03 09:30:41 +0800

    add nn folder

---
+++ pyserver/server3/lib/models/nn/toy/imdb_cnn_lstm.py
@@ -0,0 +1,154 @@
+from keras.preprocessing import sequence
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation
+from keras.layers import Embedding
+from keras.layers import LSTM
+from keras.layers import Conv1D, MaxPooling1D
+from keras.datasets import imdb
+
+from keras.callbacks import LambdaCallback
+from server3.lib.models.keras_callbacks import MongoModelCheckpoint
+from server3.service import logger_service
+
+from server3.lib import Sequential
+from server3.lib import graph
+
+
+def imdb_cnn_lstm(conf, input, **kw):
+    result_sds = kw.pop('result_sds', None)
+    project_id = kw.pop('project_id', None)
+    f = conf['fit']
+    e = conf['evaluate']
+    x_train = input['x_tr']
+    y_train = input['y_tr']
+    x_val = input['x_te']
+    y_val = input['y_te']
+    x_test = input['x_te']
+    y_test = input['y_te']
+
+    # Embedding
+    embedding_size = 128
+    max_features = input['max_features']
+    maxlen = input['maxlen']
+    x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
+    x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
+    x_val = x_test
+    # set parameters:
+
+    # Convolution
+    kernel_size = 5
+    filters = 64
+    pool_size = 4
+
+    # LSTM
+    lstm_output_size = 70
+
+    with graph.as_default():
+        model = Sequential()
+        model.add(Embedding(max_features, embedding_size, input_length=maxlen))
+        model.add(Dropout(0.25))
+        model.add(Conv1D(filters,
+                         kernel_size,
+                         padding='valid',
+                         activation='relu',
+                         strides=1))
+        model.add(MaxPooling1D(pool_size=pool_size))
+        model.add(LSTM(lstm_output_size))
+        model.add(Dense(1))
+        model.add(Activation('sigmoid'))
+
+        model.compile(loss='binary_crossentropy',
+                      optimizer='adam',
+                      metrics=['accuracy'])
+
+        # callback to save metrics
+        batch_print_callback = LambdaCallback(on_epoch_end=
+                                              lambda epoch, logs:
+                                              logger_service.log_epoch_end(
+                                                  epoch, logs,
+                                                  result_sds,
+                                                  project_id))
+
+        # checkpoint to save best weight
+        best_checkpoint = MongoModelCheckpoint(result_sds=result_sds,
+                                               verbose=0,
+                                               save_best_only=True)
+        # checkpoint to save latest weight
+        general_checkpoint = MongoModelCheckpoint(result_sds=result_sds,
+                                                  verbose=0)
+
+        # training
+        history = model.fit(x_train, y_train,
+                            validation_data=(x_val, y_val),
+                            callbacks=[batch_print_callback, best_checkpoint,
+                                       general_checkpoint],
+                            verbose=0,
+                            **f['args'])
+
+        score = model.evaluate(x_test, y_test, **e['args'])
+        # weights = model.get_weights()
+        config = model.get_config()
+        logger_service.log_train_end(result_sds,
+                                     model_config=config,
+                                     score=score,
+                                     history=history.history)
+
+        return {
+            'score': score,
+            'history': history.history}
+
+
+def imdb_cnn_lstm_to_str():
+    pass
+
+
+IMDB_CNN_LSTM = {
+    "fit": {
+        "data_fields": {
+            "name": "training_fields",
+            "type": {
+                "key": "transfer_box",
+                "des": "data fields for x and y",
+            },
+            "default": None,
+            "required": True,
+            "x_data_type": ['integer', 'float'],
+            "y_data_type": ['integer', 'float'],
+            "x_len_range": None,
+            "y_len_range": None
+        },
+        "args": [
+            {
+                "name": "batch_size",
+                "type": {
+                    "key": "int",
+                    "des": "Number of samples per gradient update",
+                    "range": None
+                },
+                "default": 32
+            },
+            {
+                "name": "epochs",
+                "type": {
+                    "key": "int",
+                    "des": "Number of epochs to train the model",
+                    "range": None
+                },
+                "default": 2
+            },
+        ],
+    },
+    "evaluate": {
+        "args": [
+            {
+                "name": "batch_size",
+                "type": {
+                    "key": "int",
+                    "des": "Number of samples per gradient update",
+                    "range": None
+                },
+                "default": 32
+            },
+        ]
+    }
+}
---
+++ pyserver/server3/lib/models/nn/toy/mnist_mlp.py
@@ -0,0 +1,142 @@
+# -*- coding: UTF-8 -*-
+from keras.callbacks import LambdaCallback
+from keras.layers import Dense, Dropout
+from keras.optimizers import RMSprop
+
+from server3.lib.models.keras_callbacks import MongoModelCheckpoint
+from server3.service import logger_service
+
+from server3.lib import Sequential
+from server3.lib import graph
+
+
+def mnist_mlp(conf, input, **kw):
+    result_sds = kw.pop('result_sds', None)
+    project_id = kw.pop('project_id', None)
+    f = conf['fit']
+    e = conf['evaluate']
+    x_train = input['x_tr']
+    y_train = input['y_tr']
+    x_val = input['x_te']
+    y_val = input['y_te']
+    x_test = input['x_te']
+    y_test = input['y_te']
+    num_classes = y_train.shape[1]
+
+    # 获取 img 的格式
+    x_train_shape = x_train.shape
+    if x_train_shape[1] > 3:
+        # 格式为 (None, img_rows, img_cols, 1)
+        x_train = x_train.reshape(-1, x_train_shape[1] * x_train_shape[2])
+        x_test = x_test.reshape(-1, x_train_shape[1] * x_train_shape[2])
+        x_val = x_test
+    else:
+        # 格式为 (None, 1, img_rows, img_cols)
+        x_train = x_train.reshape(-1, x_train_shape[2] * x_train_shape[3])
+        x_test = x_test.reshape(-1, x_train_shape[2] * x_train_shape[3])
+        x_val = x_test
+    # print(x_train.shape)
+    with graph.as_default():
+        model = Sequential()
+        model.add(Dense(512, activation='relu',
+                        input_shape=(x_train.shape[1],)))
+        model.add(Dropout(0.2))
+        model.add(Dense(512, activation='relu'))
+        model.add(Dropout(0.2))
+        model.add(Dense(num_classes, activation='softmax'))
+
+        model.compile(loss='categorical_crossentropy',
+                      optimizer=RMSprop(),
+                      metrics=['accuracy'])
+
+        # callback to save metrics
+        batch_print_callback = LambdaCallback(on_epoch_end=
+                                              lambda epoch, logs:
+                                              logger_service.log_epoch_end(
+                                                  epoch, logs,
+                                                  result_sds,
+                                                  project_id))
+
+        # checkpoint to save best weight
+        best_checkpoint = MongoModelCheckpoint(result_sds=result_sds,
+                                               verbose=0,
+                                               save_best_only=True)
+        # checkpoint to save latest weight
+        general_checkpoint = MongoModelCheckpoint(result_sds=result_sds,
+                                                  verbose=0)
+
+        # training
+        history = model.fit(x_train, y_train,
+                            validation_data=(x_val, y_val),
+                            callbacks=[batch_print_callback, best_checkpoint,
+                                       general_checkpoint],
+                            verbose=0,
+                            **f['args'])
+
+        score = model.evaluate(x_test, y_test, **e['args'])
+        # weights = model.get_weights()
+        config = model.get_config()
+        logger_service.log_train_end(result_sds,
+                                     model_config=config,
+                                     score=score,
+                                     history=history.history)
+
+        return {
+            'score': score,
+            'history': history.history}
+
+
+def mnist_mlp_to_str():
+    pass
+
+
+MNIST_MLP = {
+    "fit": {
+        "data_fields": {
+            "name": "training_fields",
+            "type": {
+                "key": "transfer_box",
+                "des": "data fields for x and y",
+            },
+            "default": None,
+            "required": True,
+            "x_data_type": ['integer', 'float'],
+            "y_data_type": ['integer', 'float'],
+            "x_len_range": None,
+            "y_len_range": None
+        },
+        "args": [
+            {
+                "name": "batch_size",
+                "type": {
+                    "key": "int",
+                    "des": "Number of samples per gradient update",
+                    "range": None
+                },
+                "default": 128
+            },
+            {
+                "name": "epochs",
+                "type": {
+                    "key": "int",
+                    "des": "Number of epochs to train the model",
+                    "range": None
+                },
+                "default": 20
+            },
+        ],
+    },
+    "evaluate": {
+        "args": [
+            {
+                "name": "batch_size",
+                "type": {
+                    "key": "int",
+                    "des": "Number of samples per gradient update",
+                    "range": None
+                },
+                "default": 32
+            },
+        ]
+    }
+}
