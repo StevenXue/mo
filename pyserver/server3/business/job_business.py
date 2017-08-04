e281cd47715f4518b498fa0e1e964704215abad0
小天 <sky@shoutens-MacBook-Pro.local>
2017-08-04 13:33:29 +0800

    json save job

--- pyserver/server3/business/job_business.py
+++ pyserver/server3/business/job_business.py
@@ -52,7 +52,7 @@ def get_by_job_status(job_status):
     return job_repo.read_by_unique_field('job_status', job_status)
 
 
-def add_toolkit_job(toolkit_obj, staging_data_set_obj, project_obj, *argv):
+def add_toolkit_job(toolkit_obj, staging_data_set_obj, project_obj, **kwargs):
     """toolkit is a obj"""
     # job = job_obj['staging_data_set'] or job_obj['model'] or job_obj['toolkit']
     # if not 0 < len(toolkit_obj.items()) <= 1:
@@ -62,7 +62,7 @@ def add_toolkit_job(toolkit_obj, staging_data_set_obj, project_obj, *argv):
     job_obj = Job(status=0, toolkit=toolkit_obj,
                   staging_data_set=staging_data_set_obj,
                   project=project_obj,
-                  create_time=time, fields=argv if argv else None)
+                  create_time=time, **kwargs)
     return job_repo.create(job_obj)
 
 
