659a50287985317a119d52c8f45dbf276514dd04
小天 <sky@shoutens-MacBook-Pro.local>
2017-08-09 19:19:22 +0800

    no message

--- pyserver/server3/service/visualization_service.py
+++ pyserver/server3/service/visualization_service.py
@@ -119,10 +119,7 @@ def usr_story2_exploration(data, d_type, sds_id):
         table_data = json_utility.me_obj_list_to_dict_list(staging_data_business.get_by_staging_data_set_id_limit(ObjectId(sds_id), 5))
         data.update({"table": table_data})
     elif d_type == 2:
-        data.update({
-            "1": None,
-            "2": None
-        })
+        pass
     else:
         pass
 
