3d0fd6193b31a08f6c7b190793f42959ab2b40da
taoyiran <gerardtao0315@gmail.com>
2017-08-10 00:39:47 +0800

    hyper param fixed 3

--- client/src/routes/project/model/modelForms.js
+++ client/src/routes/project/model/modelForms.js
@@ -37,7 +37,8 @@ export default class ModelForms extends React.Component {
       isActive: true,
       params: this.props.params,
       selectedFile: '',
-      spliter: {}
+      spliter: {},
+      hyped: false,
     }
   }
 
@@ -172,6 +173,10 @@ export default class ModelForms extends React.Component {
 
   onClickRun(){
     let run_params = this.constructParams();
+    console.log(this.state.hyped, run_params);
+    if(run_params['compile']['loss']['distribute'] || run_params['compile']['optimizer']['distribute']){
+      console.log('hype');
+    }
     let params = {};
     if(this.state.fit.data_fields) {
       params = Object.assign({
@@ -203,7 +208,13 @@ export default class ModelForms extends React.Component {
     }else{
       this.setState({visible: true});
       if(!this.state.end){
-        fetch(flaskServer + '/model/models/run/' + this.props.model_id, {
+        let url = '';
+        if(this.state.hyped){
+          url = flaskServer + 'model/models/run_hyperas_model/' + this.props.model_id
+        }else{
+          url = flaskServer + '/model/models/run/' + this.props.model_id
+        }
+        fetch(url, {
           method: 'post',
           headers: {
             'Content-Type': 'application/json',
@@ -268,6 +279,7 @@ export default class ModelForms extends React.Component {
                   <Icon type="close" style={{ fontSize: 10, color: '#CC241C' }} onClick={() => this.onDeleteLayer(el)} />
                   <div style={{width: '90%', marginLeft: 5}}>
                   <Layer layers={this.state.layer} isActive={this.state.isActive}
+                         setHype={(value) => this.setState({hyped: value})}
                          index={index}
                          getParams={(value) => this.getParams(el, value)}/>
                   </div>
@@ -326,6 +338,7 @@ export default class ModelForms extends React.Component {
           <div>
             <p style={{color: '#108ee9'}}>Compile</p>
             <Compile compile={this.state.compile['args']} isActive={this.state.isActive}
+                     setHype={(value) => this.setState({hyped: value})}
                      getParams={(value) => this.getCompileParams(value)}/>
           </div>
           }
