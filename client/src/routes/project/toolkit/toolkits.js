46ad3a16e357c65f59631884ef6afc1ab647a363
taoyiran <gerardtao0315@gmail.com>
2017-08-10 14:53:14 +0800

    fix tk

--- client/src/routes/project/toolkit/toolkits.js
+++ client/src/routes/project/toolkit/toolkits.js
@@ -374,7 +374,7 @@ export default class Toolkit extends React.Component {
                      onOk={() => this.setState({visible: false})}
                      onCancel={() => this.setState({visible: false})}
               >
-                <VisualizationPanel visual_sds_id={"598b28c4e89bdecfe8553170"} />
+                <VisualizationPanel visual_sds_id={this.state.visual_sds_id} />
               </Modal>
             </div>
           </div>
