e92291d7d77bb4fe5233f77f9b87a601f71dd06f
taoyiran <gerardtao0315@gmail.com>
2017-08-16 16:36:04 +0800

    partially fix notebook

--- client/src/routes/project/preprocess/columnOperation.js
+++ client/src/routes/project/preprocess/columnOperation.js
@@ -147,7 +147,7 @@ export default class ColumnOperations extends React.Component {
                   <div style={{float: 'right', marginTop: -20, marginRight: -20}}>
                     <Popover content={
                       <ColumnPreview stagedDs={this.state.dataSet} name={e} type={dataSet[e]}/>
-                    } style={{top: 20 }} title={e} trigger="click" >
+                    } title={e} trigger="click" >
                       <Button size="small" >
                         <span style={{fontSize: '12px'}}>VIEW</span>
                       </Button>
