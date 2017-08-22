2224085b507cf9f16aeec50cb095d4f4680f52c4
Zhaofeng Li <zhaofengli@MacBook-Pro-LZF.local>
2017-08-22 21:52:04 +0800

    add export model version if version exists

--- pyserver/server3/service/saved_model_services/keras_saved_model.py
+++ pyserver/server3/service/saved_model_services/keras_saved_model.py
@@ -12,6 +12,7 @@ from tensorflow.python.saved_model import builder as saved_model_builder
 from tensorflow.python.saved_model import tag_constants
 from tensorflow.python.saved_model.signature_def_utils_impl import \
     predict_signature_def
+from tensorflow.python.lib.io import file_io
 
 load_model = load_model
 model_from_json = model_from_json
@@ -38,10 +39,15 @@ def export(new_model, working_dir, export_path_base):
     tf.app.flags.DEFINE_string('work_dir', working_dir, 'Working directory.')
     FLAGS = tf.app.flags.FLAGS
 
-    # export_path_base = sys.argv[-1]
     export_path = os.path.join(
         tf.compat.as_bytes(export_path_base),
         tf.compat.as_bytes(str(FLAGS.model_version)))
+
+    # if version path exists, create a new version
+    if file_io.file_exists(export_path):
+        export_path = os.path.join(
+            tf.compat.as_bytes(export_path_base),
+            tf.compat.as_bytes(str(FLAGS.model_version + 1)))
 
     builder = saved_model_builder.SavedModelBuilder(export_path)
 
