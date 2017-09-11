14ff055cc0ad9948e510b909041d797f7f9653c3
Zhaofeng Li <lzfxxx@gmail.com>
2017-09-12 00:32:17 +0800

    image classifier to code

--- pyserver/server3/lib/models/nn/applications/image_classifier_inception_v3.py
+++ pyserver/server3/lib/models/nn/applications/image_classifier_inception_v3.py
@@ -1,4 +1,5 @@
 import os
+import inspect
 
 from keras import applications
 from keras import backend as K
@@ -34,15 +35,6 @@ def image_classifier_inception_v3(conf, input, **kw):
     nb_train_samples = input['nb_train_samples']
     nb_validation_samples = input['nb_validation_samples']
 
-    # 通过train_data_dir下的文件夹数目得到分类数量
-    l = os.listdir(train_data_dir)
-    l.remove('.DS_Store')
-    num_classes = len(l)
-    if num_classes < 2:
-        raise Exception('classes should be more than 1, put your '
-                        'different classes images file into '
-                        'different folder')
-
     # dimensions of our images.
     # use 150, 150 as default
     img_width, img_height = 150, 150
@@ -53,128 +45,200 @@ def image_classifier_inception_v3(conf, input, **kw):
         input_shape = (img_width, img_height, 3)
 
     with graph.as_default():
-        # load the inception_v3 network
-        base_model = applications.InceptionV3(weights='imagenet',
-                                              include_top=False,
-                                              input_shape=input_shape)
-
-        # build the top of cnn network
-        top_model = Sequential()
-        top_model.add(Flatten(input_shape=base_model.output_shape[1:]))
-        # top_model.add(Dense(256, activation='relu'))
-        top_model.add(Dropout(0.5))
-
-        # binary class
-        if num_classes == 2:
-            top_model.add(Dense(1, activation='sigmoid'))
-            model = Model(inputs=base_model.input,
-                          outputs=top_model(base_model.output))
-
-            for layer in model.layers[:-2]:
-                layer.trainable = False
-
-            model.compile(loss='binary_crossentropy',
-                          optimizer='rmsprop',
-                          metrics=['accuracy',
-                                   custom_metrcis.matthews_correlation,
-                                   custom_metrcis.precision,
-                                   custom_metrcis.recall,
-                                   custom_metrcis.fmeasure,
-                                   ])
-        else:
-            top_model.add(Dense(num_classes, activation='softmax'))
-            model = Model(inputs=base_model.input,
-                          outputs=top_model(base_model.output))
-            for layer in model.layers[:-2]:
-                layer.trainable = False
-            model.compile(loss='categorical_crossentropy',
-                          optimizer='rmsprop',
-                          metrics=['accuracy'])
-
-        # this is the augmentation configuration we will use for training
-        train_datagen = ImageDataGenerator(
-            rescale=1. / 255,
-            shear_range=0.2,
-            zoom_range=0.2,
-            horizontal_flip=True)
-        # this is the augmentation configuration we will use for testing:
-        # only rescaling
-        test_datagen = ImageDataGenerator(rescale=1. / 255)
-
-        if num_classes == 2:
-            class_mode = 'binary'
-        else:
-            class_mode = 'categorical'
-
-        train_generator = train_datagen.flow_from_directory(
-            train_data_dir,
-            target_size=(img_width, img_height),
-            batch_size=batch_size,
-            class_mode=class_mode)
-        validation_generator = test_datagen.flow_from_directory(
-            validation_data_dir,
-            target_size=(img_width, img_height),
-            batch_size=batch_size,
-            class_mode=class_mode)
-        # callback to save metrics
-        batch_print_callback = LambdaCallback(on_epoch_begin=
-                                              lambda epoch, logs:
-                                              logger_service.log_epoch_begin(
-                                                  epoch, logs,
-                                                  result_sds,
-                                                  project_id),
-                                              on_epoch_end=
-                                              lambda epoch, logs:
-                                              logger_service.log_epoch_end(
-                                                  epoch, logs,
-                                                  result_sds,
-                                                  project_id),
-                                              on_batch_end=
-                                              lambda batch, logs:
-                                              logger_service.log_batch_end(
-                                                  batch, logs,
-                                                  result_sds,
-                                                  project_id)
-                                              )
-
-        # checkpoint to save best weight
-        best_checkpoint = MyModelCheckpoint(
-            ['/pyserver/best.hdf5',
-             os.path.abspath(os.path.join(result_dir, 'best.hdf5'))],
-            save_weights_only=True,
-            verbose=1, save_best_only=True)
-        # checkpoint to save latest weight
-        general_checkpoint = MyModelCheckpoint(
-            ['/pyserver/latest.hdf5', os.path.abspath(os.path.join(result_dir,
-                                                                   'latest.hdf5'))],
-            save_weights_only=True,
-            verbose=1)
-
-        history = model.fit_generator(
-            train_generator,
-            steps_per_epoch=nb_train_samples // batch_size,
-            epochs=epochs,
-            validation_data=validation_generator,
-            validation_steps=nb_validation_samples // batch_size,
-            callbacks=[batch_print_callback, best_checkpoint,
-                       general_checkpoint],
-        )
-        # model.save_weights('first_try.h5')
-        config = model.get_config()
-        logger_service.log_train_end(result_sds,
-                                     model_config=config,
-                                     # score=score,
-                                     history=history.history)
-
-        model.save_weights(result_dir + 'final.hdf5')
-        with open(result_dir + 'model.json', 'w') as f:
-            f.write(model.to_json())
-
-        return {'history': history.history}
+        return model_main(result_sds, project_id, result_dir, train_data_dir,
+                          validation_data_dir, nb_train_samples,
+                          nb_validation_samples, input_shape,
+                          img_width, img_height,
+                          epochs, batch_size)
 
 
-def image_classifier_inception_v3_to_str():
-    pass
+def model_main(result_sds, project_id, result_dir, train_data_dir,
+               validation_data_dir, nb_train_samples,
+               nb_validation_samples, input_shape,
+               img_width, img_height,
+               epochs, batch_size):
+
+    # 通过train_data_dir下的文件夹数目得到分类数量
+    l = os.listdir(train_data_dir)
+    l.remove('.DS_Store')
+    num_classes = len(l)
+    if num_classes < 2:
+        raise Exception('classes should be more than 1, put your '
+                        'different classes images file into '
+                        'different folder')
+
+    # load the inception_v3 network
+    base_model = applications.InceptionV3(weights='imagenet',
+                                          include_top=False,
+                                          input_shape=input_shape)
+
+    # build the top of cnn network
+    top_model = Sequential()
+    top_model.add(Flatten(input_shape=base_model.output_shape[1:]))
+    # top_model.add(Dense(256, activation='relu'))
+    top_model.add(Dropout(0.5))
+
+    # binary class
+    if num_classes == 2:
+        top_model.add(Dense(1, activation='sigmoid'))
+        model = Model(inputs=base_model.input,
+                      outputs=top_model(base_model.output))
+
+        for layer in model.layers[:-2]:
+            layer.trainable = False
+
+        model.compile(loss='binary_crossentropy',
+                      optimizer='rmsprop',
+                      metrics=['accuracy',
+                               custom_metrcis.matthews_correlation,
+                               custom_metrcis.precision,
+                               custom_metrcis.recall,
+                               custom_metrcis.fmeasure,
+                               ])
+    else:
+        top_model.add(Dense(num_classes, activation='softmax'))
+        model = Model(inputs=base_model.input,
+                      outputs=top_model(base_model.output))
+        for layer in model.layers[:-2]:
+            layer.trainable = False
+        model.compile(loss='categorical_crossentropy',
+                      optimizer='rmsprop',
+                      metrics=['accuracy'])
+
+    # this is the augmentation configuration we will use for training
+    train_datagen = ImageDataGenerator(
+        rescale=1. / 255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+    # this is the augmentation configuration we will use for testing:
+    # only rescaling
+    test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+    if num_classes == 2:
+        class_mode = 'binary'
+    else:
+        class_mode = 'categorical'
+
+    train_generator = train_datagen.flow_from_directory(
+        train_data_dir,
+        target_size=(img_width, img_height),
+        batch_size=batch_size,
+        class_mode=class_mode)
+    validation_generator = test_datagen.flow_from_directory(
+        validation_data_dir,
+        target_size=(img_width, img_height),
+        batch_size=batch_size,
+        class_mode=class_mode)
+    # callback to save metrics
+    batch_print_callback = LambdaCallback(on_epoch_begin=
+                                          lambda epoch, logs:
+                                          logger_service.log_epoch_begin(
+                                              epoch, logs,
+                                              result_sds,
+                                              project_id),
+                                          on_epoch_end=
+                                          lambda epoch, logs:
+                                          logger_service.log_epoch_end(
+                                              epoch, logs,
+                                              result_sds,
+                                              project_id),
+                                          on_batch_end=
+                                          lambda batch, logs:
+                                          logger_service.log_batch_end(
+                                              batch, logs,
+                                              result_sds,
+                                              project_id)
+                                          )
+
+    # checkpoint to save best weight
+    best_checkpoint = MyModelCheckpoint(
+        ['/pyserver/best.hdf5',
+         os.path.abspath(os.path.join(result_dir, 'best.hdf5'))],
+        save_weights_only=True,
+        verbose=1, save_best_only=True)
+    # checkpoint to save latest weight
+    general_checkpoint = MyModelCheckpoint(
+        ['/pyserver/latest.hdf5', os.path.abspath(os.path.join(result_dir,
+                                                               'latest.hdf5'))],
+        save_weights_only=True,
+        verbose=1)
+
+    history = model.fit_generator(
+        train_generator,
+        steps_per_epoch=nb_train_samples // batch_size,
+        epochs=epochs,
+        validation_data=validation_generator,
+        validation_steps=nb_validation_samples // batch_size,
+        callbacks=[batch_print_callback, best_checkpoint,
+                   general_checkpoint],
+    )
+    # model.save_weights('first_try.h5')
+    config = model.get_config()
+    logger_service.log_train_end(result_sds,
+                                 model_config=config,
+                                 # score=score,
+                                 history=history.history)
+
+    model.save_weights(result_dir + 'final.hdf5')
+    with open(result_dir + 'model.json', 'w') as f:
+        f.write(model.to_json())
+
+    return {'history': history.history}
+
+
+def image_classifier_inception_v3_to_str(conf, head_str, **kw):
+    # extract conf
+    f = conf['fit']['args']
+    e = conf['evaluate']['args']
+    epochs = f['epochs']
+    batch_size = f['batch_size']
+    # extract kw
+    result_sds = kw.pop('result_sds', None)
+    project_id = kw.pop('project_id', None)
+
+    # dimensions of our images.
+    # use 150, 150 as default
+    img_width, img_height = 150, 150
+
+    if K.image_data_format() == 'channels_first':
+        input_shape = (3, img_width, img_height)
+    else:
+        input_shape = (img_width, img_height, 3)
+
+    if result_sds is None:
+        raise RuntimeError('no result_sds created')
+
+    str_model = 'from keras.models import Sequential\n'
+    str_model += 'from keras.callbacks import LambdaCallback\n'
+    str_model += 'from keras.preprocessing.image import ImageDataGenerator\n'
+    str_model += 'from keras.layers import Conv2D, MaxPooling2D\n'
+    str_model += 'from keras.layers import Activation, Dropout, Flatten, Dense\n'
+    str_model += 'from keras import backend as K\n'
+    str_model += 'from keras.optimizers import SGD\n'
+    str_model += 'from server3.lib.models.keras_callbacks import ' \
+                 'MongoModelCheckpoint\n'
+    str_model += 'from server3.service import logger_service\n'
+    str_model += 'from server3.service import job_service\n'
+    str_model += 'from server3.business import staging_data_set_business\n'
+    str_model += head_str
+    str_model += "result_sds = staging_data_set_business.get_by_id('%s')\n" % \
+                 result_sds['id']
+    str_model += "project_id = '%s'\n" % project_id
+    str_model += "epochs = %s\n" % epochs
+    str_model += "batch_size = %s\n" % batch_size
+    str_model += "img_width = %s\n" % img_width
+    str_model += "img_height = %s\n" % img_height
+    str_model += "input_shape = (%s, %s, %s)\n" % input_shape
+    model_main_str = inspect.getsource(model_main)
+    str_model += model_main_str
+    str_model += 'print(model_main(result_sds, project_id, train_data_dir, ' \
+                 'validation_data_dir, nb_train_samples, ' \
+                 'nb_validation_samples, input_shape, ' \
+                 'img_width, img_height, ' \
+                 'epochs, batch_size, num_classes))\n'
+    print(str_model)
+    return str_model
 
 
 IMAGE_CLASSIFIER_INCEPTION_V3 = {
