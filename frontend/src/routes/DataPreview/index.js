f05eb42bbe28dc3938df78afe84746165005c798
rayblade <lizheray@gmail.com>
2017-10-26 13:45:23 +0800

    for merge

--- frontend/src/routes/DataPreview/index.js
+++ frontend/src/routes/DataPreview/index.js
@@ -1,57 +1,63 @@
 import React from 'react'
-import { Card, Table } from 'antd'
+import { Card, Table, Checkbox } from 'antd'
 import { connect } from 'dva'
 import styles from './index.css'
 
-const columns = [{
-  title: 'Name',
-  dataIndex: 'name',
-  render: text => <a href="#">{text}</a>,
-}, {
-  title: 'Age',
-  dataIndex: 'age',
-}, {
-  title: 'Address',
-  dataIndex: 'address',
-}];
-const data = [{
-  key: '1',
-  name: 'John Brown',
-  age: 32,
-  address: 'New York No. 1 Lake Park',
-}, {
-  key: '2',
-  name: 'Jim Green',
-  age: 42,
-  address: 'London No. 1 Lake Park',
-}, {
-  key: '3',
-  name: 'Joe Black',
-  age: 32,
-  address: 'Sidney No. 1 Lake Park',
-}, {
-  key: '4',
-  name: 'Disabled User',
-  age: 99,
-  address: 'Sidney No. 1 Lake Park',
-}];
+class DataPreview extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      ...props
+    }
 
-function DataPreview({ location }) {
-  return (
-    <div>
-      <Card title="File Information" className={styles.card} >
-        <p>1000 rows, 1000 lines, 298 records, 13 missing</p>
-      </Card>
-      <div className={styles.tableback}>
-        <Table dataSource={data} columns={columns} bordered='true'/>
+  }
 
+  getWidth (columns) {
+    return 100 * columns.length
+  }
+
+  render () {
+    const ds = this.props.upload.dataSet
+    let dsColumns
+    let old_col
+    let new_col
+    if (Array.isArray(ds) && ds.length > 0) {
+      dsColumns = Object.keys(ds[0]).filter((el) => el !== 'data_set')
+      dsColumns = dsColumns.filter((el) => el !== '_id')
+      dsColumns = dsColumns.filter((el) => el !== 'staging_dataset_id')
+      old_col = dsColumns.map((e) => ({
+          title: <div>{e}</div>,
+          width: 100,
+          dataIndex: e,
+          key: e,
+        }),
+      )
+      new_col = dsColumns.map((e) => (<span>{e}</span>),
+      )
+
+    }
+
+    return (
+      <div>
+        <Card title="File Information" className={styles.card} >
+          <p>1000 rows, 1000 lines, 298 records, 13 missing</p>
+        </Card>
+        <div>
+          {new_col}
+        </div>
+
+        <div className={styles.tableback}>
+          <Table style={{ marginTop: 5, width: '100%' }}
+                 dataSource={ds}
+                 columns={old_col}
+                 pagination={false}
+                 bordered={true}
+                 scroll={{ x: this.getWidth(old_col), y: '100%' }}
+          />
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
-function mapStateToProps() {
-  return {}
-}
-
-export default connect(mapStateToProps)(DataPreview)
+export default connect(({ upload }) => ({ upload }))(DataPreview)
