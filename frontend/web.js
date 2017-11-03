470e628eefe7c9ea063a497d32c1a7604279a580
Zhaofeng Li <lzfxxx@gmail.com>
2017-11-03 14:50:43 +0800

    prod redirect

--- frontend/web.js
+++ frontend/web.js
@@ -6,6 +6,10 @@ app.use(express.static('./dist'));
 // static folder for media
 app.use(express.static('./assets'));
 
+app.use('/api', function (req, res) {
+  var url = 'http://122.224.116.44:5005' + req.url;
+  req.pipe(request(url)).pipe(res);
+});
 
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, './dist', 'index.html'));
