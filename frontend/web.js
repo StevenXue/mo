b3638cee59a4c0378eaebf17cdd640c0fa21b0f5
Zhaofeng Li <lzfxxx@gmail.com>
2018-03-26 21:36:49 +0800

    deploy proxy

--- frontend/web.js
+++ frontend/web.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const path = require('path');
+const proxy = require('express-http-proxy');
 const app = express();
+
+const flaskUrl = 'http://localhost:5005';
+const hubUrl = 'http://localhost:8000';
 
 app.use(express.static('./dist'));
 // static folder for mediaosos
 app.use(express.static('./assets'));
+
+app.use('/pyapi', proxy(flaskUrl, {
+  proxyReqPathResolver: function(req) {
+    return require('url').parse(req.url).path;
+  }
+}));
+app.use('/hub_api', proxy(hubUrl, {
+  proxyReqPathResolver: function(req) {
+    return require('url').parse(req.url).path;
+  }
+}));
 
 app.use('/api', function (req, res) {
   var url = 'http://122.224.116.44:5005' + req.url;
@@ -15,4 +30,4 @@ app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, './dist', 'index.html'));
 });
 
-app.listen(8008, "0.0.0.0");
+app.listen(8989, "0.0.0.0");
