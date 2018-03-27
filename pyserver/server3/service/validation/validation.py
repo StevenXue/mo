4669a81fd83cb042e7533eff86721d08ad5763e6
Acrobaticat <acrobaticat@gmail.com>
2018-03-27 09:48:34 +0800

    added more checking function in validation.py

--- pyserver/server3/service/validation/validation.py
+++ pyserver/server3/service/validation/validation.py
@@ -9,47 +9,107 @@ class GDValidation(unittest.TestCase):
 
     MODULE_PATH = "./"
     MODULE_NAME = ""
+    MODULE_YAML_OBJ = None
 
     # Validation Step 1: File / Directory Exist
     def test_src_directory(self):
-        # print(self.MODULE_NAME)
+        '''
+            To check the [/src/] direcotary
+        :return:
+        '''
         self.assertTrue(os.path.isdir(
-            "./{}/src".format(self.MODULE_NAME)))
+            "{}/src".format(self.MODULE_PATH)),
+            msg="[/src/] directory does not exist")
 
     def test_src_checkpoint_directory(self):
-        # print(self.MODULE_NAME)
+        '''
+            To check the [/src/checkpoint/] direcotary
+        :return:
+        '''
+
         self.assertTrue(os.path.isdir(
-            "./{}/src/checkpoint".format(self.MODULE_NAME)))
+            "{}/src/checkpoint".format(self.MODULE_PATH)),
+            msg="[/src/checkpoint] directory does not exist")
 
     def test_src_data_directory(self):
-        # print(self.MODULE_NAME)
+        '''
+            To check the [/src/data/] direcotary
+        :return:
+        '''
         self.assertTrue(os.path.isdir(
-            "./{}/src/data".format(self.MODULE_NAME)))
+            "{}/src/data".format(self.MODULE_PATH)),
+            msg="[/src/data/] directory does not exist")
 
     def test_main_file(self):
-        # print(self.MODULE_NAME)
+        '''
+            To check the [/src/main.py] file
+        :return:
+        '''
         self.assertTrue(os.path.exists(
-            "./{}/src/main.py".format(self.MODULE_NAME)))
+            "{}/src/main.py".format(self.MODULE_PATH)),
+            msg="[/src/main.py] does not exist")
 
     def test_module_spec_file(self):
-        # print(self.MODULE_NAME)
+        '''
+            To check the [/src/module_spec.yml] file
+        :return:
+        '''
         self.assertTrue(os.path.exists(
-            "./{}/src/module_spec.yml".format(self.MODULE_NAME)))
+            "{}/src/module_spec.yml".format(self.MODULE_PATH)),
+            msg="[/src/module_sepc.yml] does not exist")
 
     def test_requirements_file(self):
-        # print(self.MODULE_NAME)
+        '''
+            To check the [/requirements.txt] file
+        :return:
+        '''
         self.assertTrue(os.path.exists(
-            "./{}/requiremen.txt".format(self.MODULE_NAME)), msg="asfasdfasdfasdfasdf")
+            "{}/requirements.txt".format(self.MODULE_PATH)),
+            msg="[requirements.txt] does not exist")
 
     # Validation Step 2: YAML
     def test_yaml(self):
-        with open("./{}/src/module_spec.yml".format(self.MODULE_NAME)) as stream:
-            obj = yaml.load(stream)
-            self.assertIsNotNone(obj, msg="asdfasdf")
-            yaml_input = obj.get("asdfads")
-            yaml_ouput = obj.get("output")
-            self.assertIsNotNone(yaml_input, msg="ffffff")
-            self.assertIsNotNone(yaml_ouput)
+        '''
+            To check the content of [module_spec.yaml] file
+        :return:
+        '''
+
+
+        # Check yml file can be loaded
+        with open("{}/src/module_spec.yml".format(self.MODULE_PATH)) as stream:
+            try:
+                GDValidation.MODULE_YAML_OBJ = yaml.load(stream)
+            except Exception as e:
+                self.fail(msg="yaml cannot be loaded")
+
+            # Check [input] section
+            with self.subTest(name="[input] section"):
+                self.assertIsNotNone(
+                    GDValidation.MODULE_YAML_OBJ.get("input"),
+                    msg="[input] section missing in module_spec.yml")
+
+            with self.subTest(name="[input:predict] section"):
+                yaml_input_predict = GDValidation.MODULE_YAML_OBJ.get(
+                    "input", {}).get("predict", None)
+                self.assertIsNotNone(
+                    yaml_input_predict,
+                    msg="[input/predict] section missing in module_spec.yml")
+
+            required_predict_items = ["name", "value_type", "range", "type"]
+            for k, v in yaml_input_predict.items():
+                for item in required_predict_items:
+                    with self.subTest(name="[input:predict:{}]".format(k)):
+                        self.assertIsNotNone(
+                            v.get(item, None),
+                            msg="[{}/{}] missing in module_spec.yml".format(
+                                k, item))
+
+            # Check [output] section
+            with self.subTest(name="[output] section"):
+                self.assertIsNotNone(
+                    GDValidation.MODULE_YAML_OBJ.get("output"),
+                    msg="[output] section missing in module_spec.yml")
+
 
     @classmethod
     def run_test(cls, MODULE_PATH, MODULE_NAME):
@@ -58,6 +118,7 @@ class GDValidation(unittest.TestCase):
 
         test_suite = unittest.TestLoader().loadTestsFromTestCase(cls)
         return TextTestRunner().run(test_suite)
+
 
 
 if __name__ == '__main__':
@@ -70,15 +131,19 @@ if __name__ == '__main__':
     # print(GDValidation.MODULE_NAME)
     # print(GDValidation.MODULE_PATH)
 
-    GDValidation.MODULE_PATH = "./"
+    GDValidation.MODULE_PATH = "/Users/Chun/Documents/workspace/goldersgreen/pyserver/user_directory/zhaofengli/sesese"
     GDValidation.MODULE_NAME = "sesese"
+    # /Users/Chun/Documents/workspace/goldersgreen/pyserver/user_directory/zhaofengli/sesese
     # unittest.main()
 
     test_suite = unittest.TestLoader().loadTestsFromTestCase(GDValidation)
     test_result = TextTestRunner().run(test_suite)
-    print("test_result.__dict__", test_result.__dict__)
+
+    print("total_errors", len(test_result.errors))
     print("total_failures", len(test_result.failures))
-    print("test_resultXXX", test_result.failures[0][1])
+
+    # print("test_result.__dict__", test_result.__dict__)
+    # print("test_resultXXX", test_result.failures[0][1])
 
 
 # import unittest
