79f0335fba9d66d70ef0c79b089238a25c968a94
Zhaofeng Li <lzfxxx@gmail.com>
2018-04-13 21:45:17 +0800

    vir path for prod

--- pyserver/install_venv.sh
+++ pyserver/install_venv.sh
@@ -2,9 +2,9 @@
 M_DIR="$1"
 REQ_TXT=${M_DIR}/requirements.txt
 
-export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
-export VIRTUALENVWRAPPER_VIRTUALENV=/usr/local/bin/virtualenv
-source /usr/local/bin/virtualenvwrapper.sh
+export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3.6
+export VIRTUALENVWRAPPER_VIRTUALENV=/usr/bin/virtualenv
+source /usr/bin/virtualenvwrapper.sh
 
 if [ ! -d ${M_DIR} ]; then
     echo "No such directory: $M_DIR"
