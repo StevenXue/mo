6a2eb332c71e0ca48e97216092688e60749afa22
Weston Lu <westonlu@foxmail.com>
2018-04-16 09:52:26 +0800

    nc

--- pyserver/server3/route/user_route.py
+++ pyserver/server3/route/user_route.py
@@ -71,7 +71,8 @@ def register():
     if user_ID is None or password is None or code is None:
         return jsonify({'response': 'invalid user or password'}), 400
     try:
-        added_user = user_service.register(user_ID, password, phone, code, **data)
+        added_user = user_service.register(user_ID, password, phone, code,
+                                           **data)
         added_user = json_utility.convert_to_json(added_user.to_mongo())
         added_user.pop('password')
         return jsonify({'response': added_user}), 200
@@ -113,7 +114,8 @@ def reset_password():
     code = data.pop("code")
     new_password = data.pop("new_password")
     try:
-        user = user_service.reset_password(phone, message_id, code, new_password)
+        user = user_service.reset_password(phone, message_id, code,
+                                           new_password)
     except Error as e:
         return jsonify({
             "message": e.args[0]
@@ -145,6 +147,7 @@ def login():
                              'user': user_obj}}
     return jsonify(response), 200
 
+
 @user_app.route('/forgot', methods=['GET'])
 def forgot():
     email = request.args.get('email', None)
@@ -160,12 +163,13 @@ def forgot():
     response = {'response': {'token': create_access_token(identity=user)}}
     return jsonify(response), 200
 
+
 @user_app.route('/newpassword', methods=['GET'])
 def newpassword():
     password = request.args.get('password', None)
     email = request.args.get('email', None)
     try:
-        user = user_service.newpassword_send(password,email)
+        user = user_service.newpassword_send(password, email)
         # user_obj.pop('password')
     except DoesNotExist as e:
         return jsonify({'response': '%s: %s' % (str(
@@ -175,6 +179,7 @@ def newpassword():
     # Identity can be any data that is json serializable
     response = {'response': {'token': create_access_token(identity=user)}}
     return jsonify(response), 200
+
 
 @user_app.route('/login_with_phone', methods=['POST'])
 def login_with_phone():
@@ -263,8 +268,6 @@ def set_action_entity(entity_id):
     """
     user_ID = get_jwt_identity()
     data = request.get_json()
-    print('data')
-    print(data)
     action = data.pop("action")
     entity = data.pop("entity")
 
@@ -275,7 +278,8 @@ def set_action_entity(entity_id):
         return jsonify({
             'message': "success",
             'response': {
-                "entity": json_utility.convert_to_json(result.entity.to_mongo()),
+                "entity": json_utility.convert_to_json(
+                    result.entity.to_mongo()),
                 "user": json_utility.convert_to_json(result.user.to_mongo())
             }
         }), 200
@@ -300,7 +304,7 @@ def get_action_entity():
             app.user_ID = app.user.user_ID
     else:
         for each_request in apps.objects:
-            each_request.answer_number = RequestAnswerBusiness.\
+            each_request.answer_number = RequestAnswerBusiness. \
                 answer_number_of_user_request(each_request.id)
             each_request.user_ID = each_request.user.user_ID
     return jsonify({
@@ -363,7 +367,8 @@ def get_statistics():
 
     return jsonify({
         'response': {
-            "objects": json_utility.objs_to_json_with_args(statistics.objects, ["app", "caller"]),
+            "objects": json_utility.objs_to_json_with_args(statistics.objects,
+                                                           ["app", "caller"]),
             # "objects": json_utility.me_obj_list_to_json_list(statistics.objects),
             # "objects": json.loads(statistics.objects.to_json()),
 
