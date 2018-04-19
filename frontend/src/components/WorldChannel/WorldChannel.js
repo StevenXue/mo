c17528443d53ae1a1a19eb1e9467a27ea0b5f940
bingweiChen <bingwei.chen11@gmail.com>
2018-04-19 16:18:10 +0800

    #37 add optional scroll to worldChannel

--- frontend/src/components/WorldChannel/WorldChannel.js
+++ frontend/src/components/WorldChannel/WorldChannel.js
@@ -1,88 +1,162 @@
 import React, {Component} from 'react'
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'
 import {connect} from 'dva'
-import {Tabs, Input} from 'antd'
-const TabPane = Tabs.TabPane
+import {Tabs, Input, Icon} from 'antd'
 import {WorldMessages} from './index'
+import styles from './index.less'
 
-class WorldChannel extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-
-    }
-  }
+class worldChannelC extends Component {
 
   componentDidMount() {
     // To disabled submit button at the beginning.
     this.props.dispatch({
       type: "worldChannel/getWorldMessages",
       payload: {
-        channel: "request"
+        channel: "all"
       }
     })
+  }
 
-    this.scrollToBottom();
+  render() {
+    const {worldMessages, onClickIcon, isRight, dispatch, login} = this.props
+    if (!login.user) {
+      return (
+        <div/>
+      )
+    }
+    else {
+      return <WorldChannel worldMessages={worldMessages}
+                   onClickIcon={onClickIcon}
+                   dispatch={dispatch}
+                   isRight={isRight}
+
+      />
+    }
+  }
+}
 
 
+class WorldChannel extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  componentDidMount() {
+    this.scrollToBottom(true)
   }
 
   componentDidUpdate() {
-    console.log("componentDidUpdate")
-    this.scrollToBottom();
+    this.scrollToBottom(false)
   }
 
-  scrollToBottom = () => {
-    const messagesContainer = ReactDOM.findDOMNode(this.scrollView);
-    messagesContainer.scrollTop = messagesContainer.scrollHeight;
-  };
+  scrollToBottom = (force=true) => {
+    const messagesContainer = ReactDOM.findDOMNode(this.scrollView)
+    if(messagesContainer.scrollHeight-messagesContainer.scrollTop <= messagesContainer.clientHeight+50+20 || force){
+      messagesContainer.scrollTop = messagesContainer.scrollHeight - messagesContainer.clientHeight
+    }
 
+  }
 
   handleSendMessage = (e) => {
     const inputMessage = e.target.value
     this.props.dispatch({
       type: "worldChannel/sendMessage",
       payload: {
-        channel: "request",
+        channel: "all",
         message: inputMessage
       }
     })
     e.target.value = ""
   }
 
+  subHadleSendMessage = (e) => {
+    const inputMessage = e.input.value
+    this.props.dispatch({
+      type: "worldChannel/sendMessage",
+      payload: {
+        channel: "all",
+        message: inputMessage
+      }
+    })
+    e.input.value = ""
+  }
 
   render() {
-    const {worldMessages} = this.props
+    const {worldMessages, onClickIcon, isRight} = this.props
     return (
-      <div>
-        <Tabs defaultActiveKey="0">
-          <TabPane tab="request" key="0">
+      <div className={styles.container}
+           style={{width: isRight ? 300 : 50, display: "flex", flexDirection: "column"}}
+      >
+        {
+          isRight ?
+            <div className={styles.first_row}>
 
-            <WorldMessages
-              worldMessages={worldMessages}
-              ref1={(el) => { this.scrollView = el }}
-            />
+              <Icon type="arrow-right" onClick={onClickIcon}
+                    className={styles.icon_container}
+              />
 
+              <div className={styles.title}>
+                ALL
+              </div>
+
+              <Icon type="caret-down"
+                    className={styles.icon_container}
+              />
+            </div> :
+
+            <div className={styles.first_row}>
+              <Icon type="arrow-left" onClick={onClickIcon}
+                    className={styles.icon_container}
+              />
+            </div>
+        }
+        <WorldMessages
+          worldMessages={worldMessages}
+          ref1={(el) => {
+            this.scrollView = el
+          }}
+          isRight={isRight}
+        />
+        {
+          isRight &&
+          <div className={styles.input}>
             <Input
               placeholder="输入"
               onPressEnter={this.handleSendMessage}
+              id="myInput"
+              ref="myInput"
+              // ref={(el) => this.input = el}
             />
 
-          </TabPane>
+            <div
+              style={{
+                height: 30, width: 30,
+                marginLeft: 10,
+                marginRight: 10,
+                // display: "flex",
+                // justifyContent: "center",
+                // alignItem: "center",
+                // flex: 1
+              }}
+            >
+              <img
+                style={{height: 25, width: 25}}
+                src={require('../../img/icon/aircraft.png')}
+                onClick={() => {
+                  let object = this.refs.myInput
+                  this.subHadleSendMessage(object)
+                }}
+              />
+            </div>
 
-          <TabPane tab="all" key="1">Content of Tab Pane 1
-            {/*<WorldMessages worldMessages={worldMessages}/>*/}
-          </TabPane>
-
-          <TabPane tab="Tab 2" key="2">Content of Tab Pane 2</TabPane>
-          <TabPane tab="Tab 3" key="3">Content of Tab Pane 3</TabPane>
-        </Tabs>
-
-
+          </div>
+        }
       </div>
+
     )
   }
 }
 
-export default connect(({worldChannel}) => ({...worldChannel}))(WorldChannel)
+export default connect(({login, worldChannel}) => ({login, ...worldChannel}))(worldChannelC)
 
