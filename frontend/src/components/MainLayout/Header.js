bc673591851a0deeb4445fa137a624b5dc9f167e
Zhaofeng Li <lzfxxx@gmail.com>
2018-04-27 11:13:05 +0800

    css fix

--- frontend/src/components/MainLayout/Header.js
+++ frontend/src/components/MainLayout/Header.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import {connect} from 'dva'
+import { connect } from 'dva'
 
 import styles from './Header.less'
-import {Menu, Icon, Select, Input, Button, Badge} from 'antd'
-import {Link, routerRedux} from 'dva/router'
-import {FormattedMessage} from 'react-intl'
-import {config} from '../../utils'
+import { Menu, Icon, Select, Input, Button, Badge } from 'antd'
+import { Link, routerRedux } from 'dva/router'
+import { FormattedMessage } from 'react-intl'
+import { config } from '../../utils'
 // import PostRequestModal from '../../components/postRequestModal/postRequestModal'
-import {JsonToArray} from "../../utils/JsonUtils"
+import { JsonToArray } from '../../utils/JsonUtils'
 
 const Search = Input.Search
 
@@ -65,11 +65,13 @@ const menuConfig = [
     Link: '/userrequest?tab=app',
     Icon: null,
     text: 'Request',
-  }
+  },
 ]
 
+const menuStyle = {width: 70, display: 'flex', justifyContent: 'center'}
+const menuDiv = {padding: '0 2%'}
 
-function Header({location, login, history, dispatch, allRequest,message}) {
+function Header({ location, login, history, dispatch, allRequest, message }) {
   const key = '/' + location.pathname.split('/')[1]
   const toLoginPage = () => {
     if (!login.user) {
@@ -78,45 +80,45 @@ function Header({location, login, history, dispatch, allRequest,message}) {
   }
   const logout = () => {
     localStorage.removeItem('token')
-    dispatch({type: 'login/resetUser'})
+    dispatch({ type: 'login/resetUser' })
     history.push('/user/login')
   }
 
   const onClickModifyModal = (modalState) => {
     dispatch({
       type: 'allRequest/showModal',
-      payload: {modalState: modalState},
+      payload: { modalState: modalState },
     })
   }
 
   // 计算未读信息的数量
-  const numberOfUnreadMessage= () => {
+  const numberOfUnreadMessage = () => {
     let unread = 0
     JsonToArray(message.messages).forEach((item) => {
-      if (item['is_read'] === false){
+      if (item['is_read'] === false) {
         unread += 1
       }
     })
     return unread
   }
-  const divStyle = ()=>{
+  const divStyle = () => {
     let str = String(numberOfUnreadMessage())
     // console.log(str,'str')
-    return str.length>0? {
-      position:'absolute',
-      top:-9,
-      right:3,
-      backgroundColor:'#f5222d',
-      color:"#FFFFFF",
-      height:10,
-      borderRadius:10,
-      width:10,
-    }:{}
+    return str.length > 0 ? {
+      position: 'absolute',
+      top: '-25%',
+      right: '15%',
+      backgroundColor: '#f5222d',
+      color: '#FFFFFF',
+      height: 10,
+      borderRadius: 10,
+      width: 10,
+    } : {}
 
   }
   // 点击未读信息，进入详情
-  const toMessage= (e) => {
-    switch(e.message_type) {
+  const toMessage = (e) => {
+    switch (e.message_type) {
       case 'answer':
         history.push(`/userrequest/${e.user_request}?type=${e.user_request_type}`)
         break
@@ -132,7 +134,7 @@ function Header({location, login, history, dispatch, allRequest,message}) {
     }
     dispatch({
       type: 'message/readMessage',
-      payload: {receiver_id: e.receiver_id},
+      payload: { receiver_id: e.receiver_id },
     })
   }
 
@@ -151,27 +153,29 @@ function Header({location, login, history, dispatch, allRequest,message}) {
   const translatorTemp = {
     app: '应用',
     module: '模块',
-    dataset: '数据集'
+    dataset: '数据集',
   }
 
   const switchMessage = (e) => {
-    switch(e.message_type) {
+    switch (e.message_type) {
       case 'answer':
         return <p className={styles.messageP}>{`${e.user_ID}回答了您关注的需求${e.user_request_title}`}</p>
       case 'commit':
         return <p className={styles.messageP}>{`${e.user_ID} 更新了您关注的需求  ${e.user_request_title}`} 的答案</p>
       case 'deploy':
-        return <p className={styles.messageP}>{`${e.user_ID} 上线了您关注的${translatorTemp[e.project_type]}  ${e.project_name}`}</p>
+        return <p
+          className={styles.messageP}>{`${e.user_ID} 上线了您关注的${translatorTemp[e.project_type]}  ${e.project_name}`}</p>
       case 'publish':
-        return <p className={styles.messageP}>{`${e.user_ID} 发布了您关注的${translatorTemp[e.project_type]}  ${e.project_name}`}</p>
+        return <p
+          className={styles.messageP}>{`${e.user_ID} 发布了您关注的${translatorTemp[e.project_type]}  ${e.project_name}`}</p>
       case 'deploy_request':
-        return <p className={styles.messageP}>{`${e.user_ID} 为您的答案${e.user_request_title} 上线了${translatorTemp[e.project_type]}  ${e.project_name}`}</p>
+        return <p
+          className={styles.messageP}>{`${e.user_ID} 为您的答案${e.user_request_title} 上线了${translatorTemp[e.project_type]}  ${e.project_name}`}</p>
       case 'publish_request':
-        return <p className={styles.messageP}>{`${e.user_ID} 为您的答案${e.user_request_title} 发布了${translatorTemp[e.project_type]}  ${e.project_name}`}</p>
+        return <p
+          className={styles.messageP}>{`${e.user_ID} 为您的答案${e.user_request_title} 发布了${translatorTemp[e.project_type]}  ${e.project_name}`}</p>
     }
   }
-
-
 
   return <div className={styles.container}>
     <div className={styles.box}>
@@ -182,78 +186,82 @@ function Header({location, login, history, dispatch, allRequest,message}) {
         theme='dark'
         selectedKeys={[key]}
       >
-        <Menu.Item key='logo' className={styles.logoBox}>
+        <Menu.Item key='logo' className={styles.logoBox}
+                   style={menuDiv}
+        >
           <Link to={'/'}>
             <img src={logo} className={styles.logo}/>
           </Link>
         </Menu.Item>
         {menuConfig.map(
           (e) => {
-            if (e.dropdown) {
+            // if (e.dropdown) {
+            //   return (
+            //     <SubMenu title={<span style={{width: '5%'}}>Workspace</span>} key={e.key}>
+            //       {e.dropdown.map(
+            //         (e) => {
+            //           return (
+            //             <Menu.Item key={e.key} style={{ padding: '0 2%' }}>
+            //               <div onClick={() => {
+            //                 dispatch(routerRedux.push(e.Link))
+            //               }}>
+            //                 <span style={{width: '5%'}}>{e.text}</span>
+            //               </div>
+            //             </Menu.Item>
+            //           )
+            //         })}
+            //     </SubMenu>
+            //   )
+            // } else {
               return (
-                <SubMenu title={<span>Workspace</span>} key={e.key}>
-                  {e.dropdown.map(
-                    (e) => {
-                      return (
-                        <Menu.Item key={e.key} style={{marginRight:'70px'}}>
-                          <div onClick={() => {
-                            dispatch(routerRedux.push(e.Link))
-                          }}>
-                            {e.text}
-                          </div>
-                        </Menu.Item>
-                      )
-                    })}
-                </SubMenu>
-              )
-            }
-            else {
-              return (
-                <Menu.Item key={e.key} style={{marginRight:'70px'}}>
+                <Menu.Item key={e.key} style={menuDiv}>
                   <Link to={e.Link}>
                     {e.Icon && <Icon type={e.Icon}/>}
-                    <FormattedMessage id={e.text} defaultMessage={e.text}/>
+                    <div style={menuStyle}>{e.text}</div>
+                    {/*<FormattedMessage id={e.text} defaultMessage={e.text} style={{width: '5%'}}/>*/}
                   </Link>
                 </Menu.Item>)
-            }
-          }
+            // }
+          },
         )}
         {
-          <Menu.Item key={"docs"}>
-            <div onClick={()=> window.location = "https://momodel.github.io/mo/#/zh-cn/quick_start" }>
-              <FormattedMessage id={"docs"} defaultMessage={"Docs"}/>
+          <Menu.Item key={'docs'}>
+            <div onClick={() => window.location = 'https://momodel.github.io/mo/#/zh-cn/quick_start'}>
+              <div style={menuStyle}>Docs</div>
+              {/*<FormattedMessage id={'docs'} defaultMessage={'Docs'}/>*/}
             </div>
           </Menu.Item>
         }
         <SubMenu
           className={styles.rightButton}
           title={
-            <div onClick={toLoginPage}>
-            {
-              login.user?<img src={login.user.avatar} style={{width:25,borderRadius:12.5,marginRight:10}}/>:null
-            }
+            <div onClick={toLoginPage} style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+              {
+                login.user ? <img src={login.user.avatar}
+                                  style={{ width: 25, borderRadius: '50%', marginRight: 10 }}/> : null
+              }
               <span> {login.user ? login.user.user_ID : 'Login'} </span>
             </div>
           }
         >
           {login.user &&
-          <Menu.Item key={'/profile'} style={{color:'black'}}>
-            <div onClick={() => history.push('/profile/'+login.user.user_ID)}>
+          <Menu.Item key={'/profile'} style={{ color: 'black' }}>
+            <div onClick={() => history.push('/profile/' + login.user.user_ID)}>
               Profile
             </div>
           </Menu.Item>
           }
 
           {login.user &&
-          <Menu.Item key={'/setting'} style={{color:'black'}}>
-            <div onClick={() => history.push('/setting/profile/'+login.user.user_ID)}>
+          <Menu.Item key={'/setting'} style={{ color: 'black' }}>
+            <div onClick={() => history.push('/setting/profile/' + login.user.user_ID)}>
               Setting
             </div>
           </Menu.Item>
           }
 
           {login.user &&
-          <Menu.Item key={'/logout'} style={{color:'black'}}>
+          <Menu.Item key={'/logout'} style={{ color: 'black' }}>
             <div onClick={logout}>
               Logout
             </div>
@@ -263,32 +271,40 @@ function Header({location, login, history, dispatch, allRequest,message}) {
         </SubMenu>
         <SubMenu
           className={styles.messageSubmenu}
-          style={{position:'absolute',top:6,right:'14%'}}
+          // style={{position:'absolute',top:6,right:'14%'}}
           title={
-            <span onClick={toLoginPage} style={{position:'relative'}}>
+            <span onClick={toLoginPage} style={{ position: 'relative' }}>
                 {/* <Badge count={login.user ? numberOfUnreadMessage() : 0}> */}
-                <Icon style={{color:'white',fontSize:'24px'}} type="message"/>
+              <Icon style={{ color: 'white', fontSize: '18px' }} type="message"/>
                 <div style={divStyle()}></div>
-                {/* </Badge> */}
+              {/* </Badge> */}
               </span>
           }
         >
-          <Menu.Item style={{color:'black',height:'250px',overflowY: 'auto'}}>
+          <Menu.Item style={{ color: 'black', height: '250px', overflowY: 'auto' }}>
             {login.user && JsonToArray(message.messages).map(e =>
-              <div onClick={() => toMessage(e) } key={e.receiver_id}
-                style={e.is_read === false?{width:'400px',margin:'0 -20px',backgroundColor: '#f0f2f5',color:'black'}:{width:'400px',margin:'0 -20px',color:'black'}}>
-              {switchMessage(e)}</div>)}
+              <div onClick={() => toMessage(e)} key={e.receiver_id}
+                   style={e.is_read === false ? {
+                     width: '400px',
+                     margin: '0 -20px',
+                     backgroundColor: '#f0f2f5',
+                     color: 'black',
+                   } : { width: '400px', margin: '0 -20px', color: 'black' }}>
+                {switchMessage(e)}</div>)}
           </Menu.Item>
-          <Menu.Item style={{color:'black'}}>
-            <div><div><Icon type="setting" />设置</div><div>查看全部提醒</div></div>
+          <Menu.Item style={{ color: 'black' }}>
+            <div>
+              <div><Icon type="setting"/>设置</div>
+              <div>查看全部提醒</div>
+            </div>
           </Menu.Item>
           {/*{login.user && JsonToArray(message.messages).map(e =>*/}
-            {/*<Menu.Item key={e._id} className={styles.messageMenuItem}*/}
-                       {/*style={e.is_read === false?{backgroundColor: '#f0f2f5',color:'black'}:{color:'black'}}>*/}
-              {/*<div onClick={() => toMessage(e)}>*/}
-                {/*{switchMessage(e)}*/}
-              {/*</div>*/}
-            {/*</Menu.Item>*/}
+          {/*<Menu.Item key={e._id} className={styles.messageMenuItem}*/}
+          {/*style={e.is_read === false?{backgroundColor: '#f0f2f5',color:'black'}:{color:'black'}}>*/}
+          {/*<div onClick={() => toMessage(e)}>*/}
+          {/*{switchMessage(e)}*/}
+          {/*</div>*/}
+          {/*</Menu.Item>*/}
           {/*)*/}
           {/*}*/}
         </SubMenu>
@@ -297,4 +313,4 @@ function Header({location, login, history, dispatch, allRequest,message}) {
   </div>
 }
 
-export default connect(({login, allRequest,message}) => ({login, allRequest,message}))(Header)
+export default connect(({ login, allRequest, message }) => ({ login, allRequest, message }))(Header)
