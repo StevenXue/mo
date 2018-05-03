09def4765f31b3cb8b66699c5752f24fb2ea3d11
KatarinaDuCouteau <15669929857@163.com>
2018-05-03 20:11:49 +0800

    #80  fixed

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {HashRouter, Route, Switch, Link, withRouter, routerRedux} from 'dva/router'
 import {Breadcrumb} from 'antd'
 import {connect} from 'dva'
-import dynamic from 'dva/dynamic'
+import dynamic from 'dva/dynamic';
 import pathToRegexp from 'path-to-regexp'
 import {get} from 'lodash'
 
@@ -10,7 +10,6 @@ import NewPassword from './routes/login/NewPassword'
 import Account from './routes/login/Account'
 import MainLayout from './components/MainLayout/MainLayout'
 import LaunchPage from './components/MainLayout/LaunchPage';
-
 const breadcrumbNameMap = {
   '/user': 'User',
   '/user/login': 'Login',
@@ -62,13 +61,13 @@ const RouterConfig = ({history, location, projectDetail, app}) => {
     app,
     models: () => [import('./models/modelling')],
     component: () => import('./routes/workspace/info/ProjectDetail'),
-  })
+  });
 
   const HomePage = dynamic({
     app,
     // models: () => [import('./models/launchpage')],
     component: () => import('./routes/HomePage'),
-  })
+  });
   const LaunchPage = dynamic({
     app,
     // models: () => [import('./models/launchpage')],
@@ -91,22 +90,22 @@ const RouterConfig = ({history, location, projectDetail, app}) => {
       path: '/explore',
       // models: () => [import('./models/modelling')],
       component: () => import('./routes/market/ProjectList'),
-    }, {
+    },{
       path: '/userrequest/:userrequestId',
       // models: () => [import('./models/allRequest')],
       component: () => import('./routes/UserRequest/UserRequestDetail'),
-    }, {
+    },{
       path: '/userrequest',
       // models: () => [import('./models/allRequest')],
       component: () => import('./routes/UserRequest/UserRequestList'),
-    }, {
+    },{
       path: '/profile/:userId',
       // models: () => [
       //   import('./models/profile'),
       //   import('./models/login'),
       // ],
       component: () => import('./routes/Profile'),
-    }, {
+    },{
       path: '/setting/profile/:userId',
       // models: () => [
       //   import('./models/profile'),
@@ -115,23 +114,25 @@ const RouterConfig = ({history, location, projectDetail, app}) => {
       component: () => import('./routes/UserInfo'),
     },
   ]
+
   return (
-    <div>
-      
-        <Switch>
-          <Route path="/user" component={Account}/>
-          <Route path="/newpassword" component={NewPassword}/>
-          <Route path="/:anything" component={() =>
-          <MainLayout location={location} history={history}>
-            <div style={{display: 'flex', flexDirection: 'column',
+    <Switch>
+      <Route path="/user" component={Account}/>
+      <Route path="/newpassword" component={NewPassword}/>
+      <Route path="/:anything" component={() =>
+        <MainLayout location={location} history={history}>
+          <div style={{
+            display: 'flex', 
+            flexDirection: 'column',
             width:location.pathname==='/launchpage'?'100%':'auto'
-            }}>
+            }}
+          >
             {/*<Breadcrumb>*/}
-            {/*{extraBreadcrumbItems}*/}
+              {/*{extraBreadcrumbItems}*/}
             {/*</Breadcrumb>*/}
             <Switch>
               {
-                routes.map(({path, ...dynamics}, key) => (
+                routes.map(({ path, ...dynamics }, key) => (
                   <Route key={key}
                          exact
                          path={path}
@@ -143,8 +144,9 @@ const RouterConfig = ({history, location, projectDetail, app}) => {
                 ))
               }
               <Route path="/explore/:projectId" render={(props) => <ProjectDetail {...props} market_use={true}/>}/>
+              <Route path="/launchpage" component={LaunchPage} location={location}/>
               {
-                routes2.map(({path, ...dynamics}, key) => (
+                routes2.map(({ path, ...dynamics }, key) => (
                   <Route key={key}
                          exact
                          path={path}
@@ -155,15 +157,12 @@ const RouterConfig = ({history, location, projectDetail, app}) => {
                   />
                 ))
               }
-              <Route path="/launchpage" component={LaunchPage} location={location}/>
-            </Switch>
-        </div>
-        </MainLayout>
-          }/>
-      <Route path="/" component={HomePage} location={location}/>
+              </Switch>
+          </div>
+        </MainLayout>}
+      />
+      <Route path="/" component={HomePage}/>
     </Switch>
-    
-    </div>
   )
 }
 
