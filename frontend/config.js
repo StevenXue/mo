de80447dc6fca7be3015601ce8f9603992a63952
Zhaofeng Li <lzfxxx@gmail.com>
2018-05-12 02:29:56 +0800

    fix frontend server ips

--- frontend/config.js
+++ frontend/config.js
@@ -3,10 +3,10 @@ module.exports = {
   // flaskServer: 'http://localhost:5005',
   // flaskServer: 'http://10.52.14.182:5005',
   // flaskServer: 'http://122.224.116.44:5005',
-  flaskServer: 'http://10.52.22.14:5005',
+  flaskServer: 'http://momodel.ai:8899/pyapi',
 
   // hubServer: 'http://localhost:8000',
-  hubServer: 'http://10.52.22.14:8000',
+  hubServer: 'http://momodel.ai:8899/hub_api',
 
   docsServer: 'http://localhost:3000',
 
