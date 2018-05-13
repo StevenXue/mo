b1a7ce1d4e5c6950dd26538f14a040936e58e4db
Zhaofeng Li <lzfxxx@gmail.com>
2018-05-13 20:51:17 +0800

    fix

--- frontend/src/components/MainLayout/Header.js
+++ frontend/src/components/MainLayout/Header.js
@@ -136,9 +136,7 @@ function Header({ location, login, history, dispatch, allRequest, message }) {
       case 'commit':
         history.push(`/workspace/${e.project_id}?type=${e.project_type}`)
         break
-      // case 'deploy':
-      //   history.push(`/workspace/${e.app_id}?type=app`)
-      //   break
+      case 'deploy':
       case 'publish':
       case 'deploy_request':
       case 'publish_request':
