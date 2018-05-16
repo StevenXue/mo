6693d35d35857a6cca92323985a53211de7451f2
Zhaofeng Li <lzfxxx@gmail.com>
2018-05-17 02:48:06 +0800

    #21 rebuild jl image, based on tf image, only use venvwrapper

--- pyserver/jupyterhub/base-notebook/freeze_venv.sh
+++ pyserver/jupyterhub/base-notebook/freeze_venv.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3.6
-export VIRTUALENVWRAPPER_VIRTUALENV=/usr/local/bin/virtualenv
-source /usr/local/bin/virtualenvwrapper.sh
+export VIRTUALENVWRAPPER_PYTHON=/opt/conda/bin/python3.6
+export VIRTUALENVWRAPPER_VIRTUALENV=/opt/conda/bin/virtualenv
+source /opt/conda/bin/virtualenvwrapper.sh
 
-workon $(workon)
+workon jlenv
 echo "freezing env"
 pip freeze > /home/jovyan/work/requirements.txt
 echo "freeze env done"
